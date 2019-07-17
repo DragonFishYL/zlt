399e86bb197a46080cd8c0dbde842b03f77d50e2
DragonFishYL <1435033280@qq.com>
2019-07-17 23:31:01 +0800

    订单、发票状态完成版

--- pages/billlist/billlist.js
+++ pages/billlist/billlist.js
@@ -4,7 +4,15 @@ require("../../utils/canvas.js");
 
 Page({
     data: {
-        billlistArr:[]
+		color:{
+			0:'#07c160',
+			1:'#333',
+			2:'#333',
+			3:'#333'
+		},
+        billlistArr:[],
+        openId: "",
+        type: ""
     },
     authorization: function() {
         var t = this;
@@ -29,19 +37,34 @@ Page({
     },
     billlistToDetail: function(b) {
 		console.log(b);
-		var billId = b.currentTarget.dataset.id;
-        wx.navigateTo({
-            url: "../billdetail/billdetail?id="+billId
-        });
+		var billId = b.currentTarget.dataset.id,type = b.currentTarget.dataset.type;
+		if(type == 1){
+			wx.navigateTo({
+				url: "../billadd/billadd?id="+billId
+			});
+		}else{
+			wx.navigateTo({
+				url: "../billdetail/billdetail?id="+billId
+			});
+		}
+        
     },
     onLoad: function() {
-        this.authorization();
+		var t = this;
+        t.authorization(); 
+        var n = wx.getStorageSync("user").openid;
+        t.setData({
+            openId: n
+        });
 		//请求
 		wx.showLoading({ title: '加载中', });
+		t.publicFunc(1);
+    },
+	publicFunc: function(type){
 		var t = this;
 		wx.request({
 		  url: e.globalData.publicUrl + '/Trip/billListPlay',
-		  data: { 'business_no': e.globalData.business_no, 'openid': wx.getStorageSync("user").openid },
+		  data: { 'business_no': e.globalData.business_no, 'openid': wx.getStorageSync("user").openid,'type':type },
 		  method: 'POST',
 		  header: {
 			'content-type': 'application/x-www-form-urlencoded' // 默认值
@@ -49,10 +72,25 @@ Page({
 		  success:function(res){
 			//将发票列表数据赋值
 		    console.log(res);
-			t.setData({billlistArr:res.data.data,});
+			t.setData({billlistArr:res.data.data,type:type});
+			
 			//关闭提示
 			wx.hideLoading();
 		  }
 		});
-    }
+	},
+	ordermodel:function(o){
+		console.log(o);
+        var a = o.currentTarget.dataset.type,colors=this.data.color;
+		//请求
+		wx.showLoading({ title: '加载中', });
+		colors[0] = '#333';
+		colors[1] = '#333';
+		colors[2] = '#333';
+		colors[a-1] = '#07c160';
+		this.setData({
+            color:colors
+        });
+		this.publicFunc(a);
+	},
 });
--- pages/out/out.js
+++ pages/out/out.js
@@ -1,10 +1,15 @@
-require("../../utils/util.js");
 var e = getApp(), t = require("../../utils/util.js"), n = (require("../../wxParse/wxParse.js"));
 
 Page({
     data: {
         logs: [],
         orderarr: [],
+		color:{
+			0:'#07c160',
+			1:'#333',
+			2:'#333',
+			3:'#333'
+		},
         openId: ""
     },
     authorization: function() {
@@ -33,9 +38,13 @@ Page({
         });
 		//请求
 		wx.showLoading({ title: '加载中', });
+		t.publicFunc(1);
+    },
+	publicFunc:function(type){//1全部 2已支付 3未支付 4已取消
+		var t = this;
 		wx.request({
 		  url: e.globalData.publicUrl + '/Trip/orderlistWX',
-		  data: { 'business_no': e.globalData.business_no, 'openid': wx.getStorageSync("user").openid },
+		  data: { 'business_no': e.globalData.business_no, 'openid': wx.getStorageSync("user").openid,'type':type},
 		  method: 'POST',
 		  header: {
 			'content-type': 'application/x-www-form-urlencoded' // 默认值
@@ -47,7 +56,22 @@ Page({
 			wx.hideLoading();
 		  }
 		});
-    },
+	},
+	ordermodel:function(o){
+		console.log(o);
+        var a = o.currentTarget.dataset.type,colors=this.data.color;
+		//请求
+		wx.showLoading({ title: '加载中', });
+		colors[0] = '#333';
+		colors[1] = '#333';
+		colors[2] = '#333';
+		colors[3] = '#333';
+		colors[a-1] = '#07c160';
+		this.setData({
+            color:colors
+        });
+		this.publicFunc(a);
+	},
     goPage: function(e) {
         var a = e.currentTarget.dataset.url;
         t.goPage(a);
