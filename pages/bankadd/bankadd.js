407f3ac458b8d028128098d23ad57bfa09aff25c
DragonFishYL <1435033280@qq.com>
2019-08-22 23:24:27 +0800

    银行卡管理页面完成,effect2刚开始

---
+++ pages/bankadd/bankadd.js
@@ -0,0 +1,99 @@
+var t = getApp(), e = require("../../utils/util.js"), n = require("../../wxParse/wxParse.js");
+
+require("../../utils/canvas.js");
+
+Page({
+    data: {
+        canShow: "",
+        hasUserInfo: !1,
+        canIUse: wx.canIUse("button.open-type.getUserInfo"),
+        userInfo: "",
+        avatarUrl: "",
+        canvasAvatarUrl: "",
+        photo: "",
+        canvasPhotoUrl: "",
+        shareImg1: "",
+        shareImg2: "",
+        nickName: "",
+		array2: ['--请选择--','增值税普通发票'],
+		objectArray2: [
+		  {
+			id: 0,
+			name: '--请选择--'
+		  },
+		  {
+			id: 1,
+			name: '增值税普通发票'
+		  }
+		],
+		index2:0
+    },
+    bindPickerChanges: function (e) {
+		this.setData({
+		  index2: e.detail.value
+		})
+    },
+    authorization: function() {
+        var t = this;
+        wx.getSetting({
+            success: function(e) {
+                e.authSetting["scope.userInfo"] && (console.info(1), wx.getUserInfo({
+                    success: function(e) {
+                        console.info(2), t.setData({
+                            userInfo: e.userInfo,
+                            avatarUrl: e.userInfo.avatarUrl,
+                            nickName: e.userInfo.nickName,
+                            photo: wx.getStorageSync("avatarUrl")
+                        }, function() {});
+                    }
+                }));
+            }
+        });
+    },
+    goPage: function(t) {
+        var n = t.currentTarget.dataset.url;
+        e.goPage(n);
+    },
+    watchTicket: function() {
+        wx.navigateTo({
+            url: "../watchTicket/watchTicket"
+        });
+    },
+    goFair: function() {
+        wx.navigateTo({
+            url: "../out/out"
+        });
+    },
+    goBill: function() {
+        wx.navigateTo({
+            url: "../bill/bill"
+        });
+    },
+    collect: function() {
+        wx.navigateTo({
+            url: "../collect/collect"
+        });
+    },
+    myshare: function() {
+        wx.navigateTo({
+            url: "../effect/effect"
+        });
+    },
+    homeGetUserInfo: function(n) {
+        e.autoLogin(t, n, this, "../index/index");
+    },
+    exhibitonGetUserInfo: function(n) {
+        e.autoLogin(t, n, this, "../exhibition/exhibition");
+    },
+    myshareGetUserInfo: function(n) {
+        e.autoLogin(t, n, this, "../person/person");
+    },
+    imageLoad: function(t) {
+        this.setData({
+            canShow: "active"
+        });
+    },
+    onLoad: function() {
+        this.authorization();
+    }
+});
