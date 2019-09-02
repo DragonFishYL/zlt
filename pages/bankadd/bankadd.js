4f2c03768f9c45f90541e6752851ea3d115be1d7
DragonFishYL <1435033280@qq.com>
2019-09-02 21:00:13 +0800

    添加银行卡v1

--- pages/bankadd/bankadd.js
+++ pages/bankadd/bankadd.js
@@ -15,6 +15,18 @@ Page({
         shareImg1: "",
         shareImg2: "",
         nickName: "",
+		array1: ['--请选择--','增值税普通发票'],
+		objectArray1: [
+		  {
+			id: 0,
+			name: '--请选择--'
+		  },
+		  {
+			id: 1,
+			name: '增值税普通发票'
+		  }
+		],
+		index1:0,
 		array2: ['--请选择--','增值税普通发票'],
 		objectArray2: [
 		  {
@@ -26,13 +38,105 @@ Page({
 			name: '增值税普通发票'
 		  }
 		],
-		index2:0
+		index2:0,
+		array3: ['--请选择--','增值税普通发票'],
+		objectArray3: [
+		  {
+			id: 0,
+			name: '--请选择--'
+		  },
+		  {
+			id: 1,
+			name: '增值税普通发票'
+		  }
+		],
+		index3:0,
+		array4: ['--请选择--','增值税普通发票'],
+		objectArray4: [
+		  {
+			id: 0,
+			name: '--请选择--'
+		  },
+		  {
+			id: 1,
+			name: '增值税普通发票'
+		  }
+		],
+		index4:0,
+		select2:'none',
+		select3:'none',
+		select4:'none',
+		bankdataid:'',
+		useraccount:'',
+		username:'',
+		userphone:'',
+		bankinfo:'',
     },
-    bindPickerChanges: function (e) {
-		this.setData({
-		  index2: e.detail.value
-		})
+    bindPickerChanges1: function (e) {
+		var index = e.detail.value,object = this.data.objectArray1,value = object[index]['id'];this.setData({index1: index,index2: 0,index3: 0,index4: 0,select2:'block',select3:'none',select4:'none'});
+		this.publiclevel(2,value);
     },
+    bindPickerChanges2: function (e) {
+		var index = e.detail.value,object = this.data.objectArray2,value = object[index]['id'];this.setData({index2: index,index3: 0,index4: 0,select3:'block',select4:'none'});
+		this.publiclevel(3,value);
+    },
+    bindPickerChanges3: function (e) {
+		var index = e.detail.value,object = this.data.objectArray3,value = object[index]['id'];this.setData({index3: index,index4: 0,select4:'block'});
+		this.publiclevel(4,value);
+    },
+    bindPickerChanges4: function (e) {
+		var index = e.detail.value,object = this.data.objectArray4,value = object[index]['id'];this.setData({index4: index,bankdataid:value});
+    },
+	publiclevel:function(level,pid){
+		var that = this;
+		wx.request({
+			url: t.globalData.publicUrl + '/Trip/v3yourbank',
+			data: { 'business_no': t.globalData.business_no, 'openid': wx.getStorageSync("user").openid,'level':level,'pid':pid },
+			method: 'POST',
+			header: {
+				'content-type': 'application/x-www-form-urlencoded' // 默认值
+			},
+			success:function(res){
+				console.log(res);
+				//关闭提示
+				wx.hideLoading();
+				if(res.data.status == 1){
+					wx.showToast({
+						title: res.data.message,
+						icon: 'success',
+						duration: 3000
+					})
+					if(level == 1){
+						that.setData({
+							array1:res.data.nameArr,
+							objectArray1:res.data.objArr
+						});
+					}else if(level == 2){
+						that.setData({
+							array2:res.data.nameArr,
+							objectArray2:res.data.objArr
+						});
+					}else if(level == 3){
+						that.setData({
+							array3:res.data.nameArr,
+							objectArray3:res.data.objArr
+						});
+					}else if(level == 4){
+						that.setData({
+							array4:res.data.nameArr,
+							objectArray4:res.data.objArr
+						});
+					}
+				}else{
+					wx.showToast({
+						title: res.data.message,
+						icon: 'none',
+						duration: 3000
+					})
+				}
+			}
+		});
+	},
     authorization: function() {
         var t = this;
         wx.getSetting({
@@ -54,46 +158,25 @@ Page({
         var n = t.currentTarget.dataset.url;
         e.goPage(n);
     },
-    watchTicket: function() {
-        wx.navigateTo({
-            url: "../watchTicket/watchTicket"
-        });
-    },
-    goFair: function() {
-        wx.navigateTo({
-            url: "../out/out"
-        });
-    },
-    goBill: function() {
-        wx.navigateTo({
-            url: "../bill/bill"
-        });
-    },
-    collect: function() {
-        wx.navigateTo({
-            url: "../collect/collect"
-        });
-    },
-    myshare: function() {
-        wx.navigateTo({
-            url: "../effect/effect"
-        });
-    },
-    homeGetUserInfo: function(n) {
-        e.autoLogin(t, n, this, "../index/index");
-    },
-    exhibitonGetUserInfo: function(n) {
-        e.autoLogin(t, n, this, "../exhibition/exhibition");
-    },
-    myshareGetUserInfo: function(n) {
-        e.autoLogin(t, n, this, "../person/person");
-    },
-    imageLoad: function(t) {
-        this.setData({
-            canShow: "active"
-        });
-    },
     onLoad: function() {
         this.authorization();
-    }
+		wx.showLoading({title:'加载中'});
+		this.publiclevel(1);
+    },
+	useraccount:function(d){
+		setData({useraccount: e.detail.value});
+	},
+	username:function(d){
+		setData({username: e.detail.value});
+	},
+	userphone:function(d){
+		setData({userphone: e.detail.value});
+	},
+	bankinfo:function(d){
+		setData({bankinfo: e.detail.value});
+	},
+	bankDetailAdd:function(){
+		var useraccount = this.data.useraccount,username = this.data.username,userphone = this.data.userphone,bankinfo = this.data.bankinfo,bankdataid = this.data.bankdataid;
+		
+	}
 });
